74fc21fbf76be428181adf330e913e2e80c4b533
ChisumSmith <91487164+ChisumSmith@users.noreply.github.com>
2021-12-25 15:38:56 +0800

    final

---
+++ 学分积查询/学分绩查询/app/src/main/java/com/example/test.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import com.example.parse_service.ScoreService
+import kotlin.coroutines.suspendCoroutine
+
+fun main(){
+
+
+        println(ScoreService.getScore("person!historyCourseGrade.html"))
+
+}
