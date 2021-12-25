3b1b3ffe957c557df382827673b893797786be1e
ChisumSmith <91487164+ChisumSmith@users.noreply.github.com>
2021-12-25 20:41:38 +0800

    学分绩

--- 学分积查询/学分绩查询/app/src/main/java/com/example/activity/ScoreActivity.kt
+++ 学分积查询/学分绩查询/app/src/main/java/com/example/activity/ScoreActivity.kt
@@ -48,34 +48,36 @@ class ScoreActivity : AppCompatActivity() {
 
 
 
-        val exa: String = "        DegreeData(course=计算机系统基础, final_result=78, grade_point=3),\n" +
-                "        DegreeData(course=大学生心理健康教育, final_result=90, grade_point=3.9),\n" +
-                "        DegreeData(course=程序设计基础Ⅲ, final_result=85, grade_point=3.7),\n" +
-                "        DegreeData(course=计算机系统基础实验, final_result=68, grade_point=2),\n" +
-                "        DegreeData(course=足球初级, final_result=92, grade_point=3.9),\n" +
-                "        DegreeData(course=中国近现代史纲要, final_result=72, grade_point=2.3),\n" +
-                "        DegreeData(course=高等数学（上）, final_result=83, grade_point=3.3),\n" +
-                "        DegreeData(course=智能时代的计算机科学, final_result=P, grade_point=),\n" +
-                "        DegreeData(course=线性代数Ⅰ, final_result=90, grade_point=3.9),\n" +
-                "        DegreeData(course=军事理论, final_result=75, grade_point=1),\n" +
-                "        DegreeData(course=程序设计基础III实验, final_result=81, grade_point=3.3),\n" +
-                "        DegreeData(course=大学英语（Ⅱ）, final_result=74, grade_point=2.3),\n" +
-                "        DegreeData(course=军事技能训练, final_result=优秀, grade_point=4),\n" +
-                "        DegreeData(course=信息技术基础认知与实践, final_result=88, grade_point=3.7),\n" +
-                "        DegreeData(course=大学美育, final_result=66, grade_point=1.7),\n" +
-                "        DegreeData(course=离散数学, final_result=78, grade_point=3),\n" +
-                "        DegreeData(course=思想道德与法治, final_result=84, grade_point=3.3),\n" +
-                "        egreeData(course=大学物理II（上）, final_result=79, grade_point=3),\n" +
-                "        DegreeData(course=数据结构（双语), final_result=83, grade_point=3.3),\n" +
-                "        DegreeData(course=篮球初级, final_result=77, grade_point=2.7),\n" +
-                "        DegreeData(course=电路基础 III, final_result=73, grade_point=2.3),\n" +
-                "        DegreeData(course=电路基础实验, final_result=81, grade_point=3.3),\n" +
-                "        DegreeData(course=大学英语核心能力（阅读）, final_result=74, grade_point=2.3),\n" +
-                "        DegreeData(course=高等数学（下）, final_result=71, grade_point=2),\n" +
-                "        DegreeData(course=大学物理实验I（上）, final_result=77, grade_point=2.7),\n" +
-                "        DegreeData(course=数据结构实验（双语), final_result=88, grade_point=3.7),\n" +
-                "        DegreeData(course=大学英语（Ⅲ）, final_result=72, grade_point=2.3),\n" +
-                "        DegreeData(course=概率论与数理统计, final_result=87, grade_point=3.7)\n"
+        val exa: String = "学分绩（百分制） 79.025\n"+
+                "课程名称 计算机系统基础\t最终成绩 78\t绩点 3\t\n" +
+                "课程名称 大学生心理健康教育\t最终成绩 90\t绩点 3.9\t\n" +
+                "课程名称 程序设计基础Ⅲ\t最终成绩 85\t绩点 3.7\t\n" +
+                "课程名称 计算机系统基础实验\t最终成绩 68\t绩点 2\t\n" +
+                "课程名称 足球初级\t最终成绩 92\t绩点 3.9\t\n" +
+                "课程名称 中国近现代史纲要\t最终成绩 72\t绩点 2.3\t\n" +
+                "课程名称 高等数学（上）\t最终成绩 83\t绩点 3.3\t\n" +
+                "课程名称 智能时代的计算机科学\t最终成绩 P\t绩点 \t\n" +
+                "课程名称 c线性代数Ⅰ\t最终成绩 90\t绩点 3.9\t\n" +
+                "课程名称 军事理论\t最终成绩 75\t绩点 1\t\n" +
+                "课程名称 程序设计基础III实验\t最终成绩 81\t绩点 3.3\t\n" +
+                "课程名称 大学英语（Ⅱ）\t最终成绩 74\t绩点 2.3\t\n" +
+                "课程名称 军事技能训练\t最终成绩 优秀\t绩点 4\t\n" +
+                "课程名称 信息技术基础认知与实践\t最终成绩 88\t绩点 3.7\t\n" +
+                "课程名称 大学美育\t最终成绩 66\t绩点 1.7\t\n" +
+                "课程名称 离散数学\t最终成绩 78\t绩点 3\t\n" +
+                "课程名称 思想道德与法治\t最终成绩 84\t绩点 3.3\t\n" +
+                "课程名称 大学物理II（上）\t最终成绩 79\t绩点 3\t\n" +
+                "课程名称 数据结构（双语)\t最终成绩 83\t绩点 3.3\t\n" +
+                "课程名称 篮球初级\t最终成绩 77\t绩点 2.7\t\n" +
+                "课程名称 电路基础 III\t最终成绩 73\t绩点 2.3\t\n" +
+                "课程名称 电路基础实验\t最终成绩 81\t绩点 3.3\t\n" +
+                "课程名称 大学英语核心能力（阅读）\t最终成绩 74\t绩点 2.3\t\n" +
+                "课程名称 高等数学（下）\t最终成绩 71\t绩点 2\t\n" +
+                "课程名称 大学物理实验I（上）\t最终成绩 77\t绩点 2.7\t\n" +
+                "课程名称 数据结构实验（双语)\t最终成绩 88\t绩点 3.7\t\n" +
+                "课程名称 大学英语（Ⅲ）\t最终成绩 72\t绩点 2.3\t\n" +
+                "课程名称 概率论与数理统计\t最终成绩 87\t绩点 3.7\t\n"
+
 
         textView.text = exa
     }
